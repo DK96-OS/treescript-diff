00609bb70b074ef5f2dd0c637f918ae522b736d0
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-07-04 17:59:07 +0200

    Test TreeScript-Diff Package: * Update test_main - add big_tree test cases

--- test/test_main.py
+++ test/test_main.py
@@ -7,7 +7,7 @@ import sys
 import pytest
 from pathlib import Path
 
-from test.conftest import get_simple_tree, get_simple_tree_add_file, PrintCollector
+from test.conftest import get_simple_tree, get_simple_tree_add_file, PrintCollector, get_big_tree
 from treescript_diff.__main__ import main
 
 
@@ -145,3 +145,53 @@ def test_main_r_simple_tree_remove_file(monkeypatch, mock_treescript_dir):
     monkeypatch.setattr(builtins, 'print', collector.get_mock_print())
     main()
     collector.assert_expected(expected)
+
+
+def test_main_default_simple_tree_to_big_tree(monkeypatch, mock_treescript_dir):
+    sys.argv = ['treescript-diff', mock_treescript_dir.initial_treescript_path, mock_treescript_dir.latest_treescript_path]
+    os.chdir(mock_treescript_dir.get_root_dir())
+    # Insert Text into Temp TreeScript Files
+    mock_treescript_dir.write_init_tree(get_simple_tree())
+    mock_treescript_dir.write_latest_tree(get_big_tree())
+    #
+    collector = PrintCollector()
+    monkeypatch.setattr(builtins, 'print', collector.get_mock_print())
+    main()
+    assert len(collector.get_output().splitlines()) == 729
+
+
+def test_main_a_simple_tree_to_big_tree(monkeypatch, mock_treescript_dir):
+    sys.argv = ['treescript-diff', '-a', mock_treescript_dir.initial_treescript_path, mock_treescript_dir.latest_treescript_path]
+    os.chdir(mock_treescript_dir.get_root_dir())
+    # Insert Text into Temp TreeScript Files
+    mock_treescript_dir.write_init_tree(get_simple_tree())
+    mock_treescript_dir.write_latest_tree(get_big_tree())
+    #
+    collector = PrintCollector()
+    monkeypatch.setattr(builtins, 'print', collector.get_mock_print())
+    main()
+    assert len(collector.get_output().splitlines()) == 727
+
+
+def test_main_r_simple_tree_to_big_tree(monkeypatch, mock_treescript_dir):
+    sys.argv = ['treescript-diff', '-r', mock_treescript_dir.initial_treescript_path, mock_treescript_dir.latest_treescript_path]
+    os.chdir(mock_treescript_dir.get_root_dir())
+    # Insert Text into Temp TreeScript Files
+    mock_treescript_dir.write_init_tree(get_simple_tree())
+    mock_treescript_dir.write_latest_tree(get_big_tree())
+    #
+    expected = """src/hello.js\n"""
+    collector = PrintCollector()
+    monkeypatch.setattr(builtins, 'print', collector.get_mock_print())
+    main()
+    collector.assert_expected(expected)
+
+
+def test_main_ar_simple_tree_to_big_tree(monkeypatch, mock_treescript_dir):
+    sys.argv = ['treescript-diff', '-ar', mock_treescript_dir.initial_treescript_path, mock_treescript_dir.latest_treescript_path]
+    os.chdir(mock_treescript_dir.get_root_dir())
+    #
+    collector = PrintCollector()
+    monkeypatch.setattr(builtins, 'print', collector.get_mock_print())
+    with pytest.raises(SystemExit, match='Added and Removed files are printed by default, separated by a blank line.'):
+        main()
