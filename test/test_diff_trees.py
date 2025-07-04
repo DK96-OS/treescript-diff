73b11208b975d05bdf6a4f04304c96e58be8b23a
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-07-04 17:01:26 +0200

    Test TreeScript-Diff Package: * Update test_diff_trees - add test cases

--- test/test_diff_trees.py
+++ test/test_diff_trees.py
@@ -1,8 +1,8 @@
 """ Testing Dictionary Files Diff Methods
 """
-from test.conftest import get_simple_tree, get_simple_tree_add_newline, get_simple_tree_add_file, get_big_tree
-
 from treescript_diff.diff_trees import diff_trees_additions, diff_trees_double, diff_trees_removals
+
+from test.conftest import get_simple_tree, get_simple_tree_add_newline, get_simple_tree_add_file, get_big_tree
 
 
 def test_diff_trees_additions_empty_simple_returns_empty():
@@ -43,6 +43,30 @@ def test_diff_trees_additions_simple_to_big():
 
 def test_diff_trees_additions_big_to_simple():
     generator = diff_trees_additions(get_big_tree(), get_simple_tree())
+    result = list(generator)
+    assert len(result) == 1
+
+
+def test_diff_trees_removals_simple_simple_returns_empty():
+    generator = diff_trees_removals(get_simple_tree(), get_simple_tree())
+    result = list(generator)
+    assert len(result) == 0
+
+
+def test_diff_trees_removals_newline():
+    generator = diff_trees_removals(get_simple_tree(), get_simple_tree_add_newline())
+    result = list(generator)
+    assert len(result) == 0
+
+
+def test_diff_trees_removals_add_file():
+    generator = diff_trees_removals(get_simple_tree(), get_simple_tree_add_file())
+    result = list(generator)
+    assert len(result) == 0
+
+
+def test_diff_trees_removals_remove_file():
+    generator = diff_trees_removals(get_simple_tree_add_file(), get_simple_tree())
     result = list(generator)
     assert len(result) == 1
 
