df52a6d4afad85cfc37216c11c8de87bd202fa91
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-10-30 13:25:33 -0400

    Input Package: * Update file_validation.py - revert copy method update

--- treescript_diff/input/file_validation.py
+++ treescript_diff/input/file_validation.py
@@ -7,41 +7,25 @@ from sys import exit
 from treescript_diff.input import _validate_name
 
 
-_FILE_SIZE_LIMIT = 8 * 1024**2 # 8 MB
-_FILE_SIZE_LIMIT_ERROR_MSG = "File larger than 8 MB Limit."
-_FILE_SYMLINK_DISABLED_MSG = "Symlink file paths are disabled."
-
-_FILE_DOES_NOT_EXIST_MSG = "The File does not Exist."
-_FILE_READ_OSERROR_MSG = "Failed to Read from File."
-
-
-def validate_input_file(file_name: str) -> str | None:
+def validate_input_file(file_name: str) -> str:
     """ Read the Input File, Validate (non-blank) data, and return Input str.
- - Max FileSize is 8 MB.
- - Symlink type file paths are disabled.
 
 **Parameters:**
  - file_name (str): The Name of the Input File.
 
 **Returns:**
- str? - The String Contents of the Input File.
+ - str - The String Contents of the Input File.
 
 **Raises:**
  SystemExit - If the File does not exist, or is empty or blank, or read failed.
     """
     file_path = Path(file_name)
+    if not file_path.exists():
+        exit(f"The tree file does not exist: {file_name}")
     try:
-        if not file_path.exists():
-            exit(_FILE_DOES_NOT_EXIST_MSG)
-        if file_path.is_symlink():
-            exit(_FILE_SYMLINK_DISABLED_MSG)
-        if file_path.lstat().st_size > _FILE_SIZE_LIMIT:
-            exit(_FILE_SIZE_LIMIT_ERROR_MSG)
-        if (data := file_path.read_text()) is not None:
-            if _validate_name(data):
-                return data
-            exit(f"This TreeScript file was empty: {file_name}")
-        # Fallthrough: return None
+        data = file_path.read_text()
     except OSError:
-        exit(_FILE_READ_OSERROR_MSG)
-    return None
+        exit(f"Failed to read string from file: {file_name}")
+    if _validate_name(data):
+        return data
+    exit(f"This TreeScript file was empty: {file_name}")
