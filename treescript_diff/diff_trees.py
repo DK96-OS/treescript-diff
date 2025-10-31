68f78cf8b50fdac0b220de3e48b0ac2edc334673
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-10-31 12:28:39 -0400

    TreeScript-Diff Package: * Update diff_trees - add hidden methods load_original and compare_files, use list extend method instead of loop and append, update docs * Remove dictionary_files_diff - merge methods into diff_trees module

--- treescript_diff/diff_trees.py
+++ treescript_diff/diff_trees.py
@@ -1,30 +1,29 @@
-""" The Difference between Trees.
+""" The Differences between Treescript Files.
 """
 from typing import Generator
 
-from .dictionary_files_diff import load_original, compare_files
+from treescript_files import generate_treescript_files
 
 
 def diff_trees_additions(a: str, b: str) -> Generator[str, None, None]:
     """ The TreeScript files that were added from a to b.
 
 **Parameters:**
- - a (str) : The original TreeScript.
- - b (str) : The updated TreeScript.
+ - a (str): The original TreeScript.
+ - b (str): The updated TreeScript.
 
 **Yields:**
  str - The elements of the diff that are added.
     """
-    files = load_original(a)
-    yield from compare_files(files, b)
+    yield from _compare_files(_load_original(a), b)
 
 
 def diff_trees_removals(a: str, b: str) -> Generator[str, None, None]:
     """ The TreeScript files that were removed from a to b.
 
 **Parameters:**
- - a (str) : The original TreeScript.
- - b (str) : The updated TreeScript.
+ - a (str): The original TreeScript.
+ - b (str): The updated TreeScript.
 
 **Yields:**
  Generator[str] - The elements of the diff that were removed.
@@ -36,17 +35,42 @@ def diff_trees_double(a: str, b: str) -> tuple[list[str], list[str]]:
     """ The difference between two TreeScript strings.
 
 **Parameters:**
- - a (str) : The original TreeScript.
- - b (str) : The updated TreeScript.
+ - a (str): The original TreeScript.
+ - b (str): The updated TreeScript.
 
 **Returns:**
  tuple[list[str], list[str]] - Container for the additions and removals.
     """
-    files: set[str] = load_original(a)
+    files: set[str] = _load_original(a)
     additions: list[str] = []
-    for n in compare_files(files, b):
-        additions.append(n)
+    additions.extend(_compare_files(files, b))
     removals: list[str] = []
-    for n in files:
-        removals.append(n)
+    removals.extend(files)
     return additions, removals
+
+
+def _load_original(
+    original_treescript: str,
+) -> set[str]:
+    return set[str](generate_treescript_files(original_treescript, None))
+
+
+def _compare_files(
+    original_files: set[str],
+    updated_tree: str
+) -> Generator[str, None, None]:
+    """ Compare a set of the original files with the updated TreeScript.
+ - Modifies the original_files set during execution, removing matches with updated_tree.
+
+**Parameters:**
+ - original_files (set): The Dictionary containing the original TreeScript files.
+ - updated_tree (str): The updated TreeScript to be compared for additions.
+
+**Yields:**
+ str - The file paths that were not found in the original TreeScript.
+    """
+    for node in generate_treescript_files(updated_tree, None):
+        if node in original_files:
+            original_files.remove(node)
+        else:
+            yield node
